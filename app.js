02b5780499d58c35c6705f8e7ab0161787febfe9
Hasan <hasaneke1000@gmail.com>
2023-07-01 01:46:51 +0300

    dıne

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express')
+var logger = require('./middleware/logger')
 var todoRouter = require('./routes/todos')
 var app = express()
 app.use(express.json())
 
 app.get('/', (req, res) =>{
-    console.log(products);
-    res.status(200).json(products)
+    res.status(200).send('<h1> home </h1>')
 })
 
-app.use('/api/todo', todoRouter)
+app.use('/api/todo',logger, todoRouter)
 
 app.listen(4000, () => {
     console.log('Server is listening on 4000')
